184ed95cf7eb915197558d5fa119accf8cbbdc36
OKukotin <oleksandr.kukotin@gmail.com>
2021-10-25 00:59:25 +0300

    Added getters and setters, modified toString() override

--- src/main/java/Status.java
+++ src/main/java/Status.java
@@ -44,6 +44,62 @@ public class Status {
     @JsonProperty("sms_cnt")
     private Integer smsCount;
 
+    public Integer getStatus() { return status; }
+
+    public void setStatus(Integer status) { this.status = status; }
+
+    public String getLastDate() { return lastDate; }
+
+    public void setLastDate(String lastDate) { this.lastDate = lastDate; }
+
+    public String getLastTimestamp() { return lastTimestamp; }
+
+    public void setLastTimestamp(String lastTimestamp) { this.lastTimestamp = lastTimestamp; }
+
+    public Integer getErrorCode() { return errorCode; }
+
+    public void setErrorCode(Integer errorCode) { this.errorCode = errorCode; }
+
+    public String getSendDate() { return sendDate; }
+
+    public void setSendDate(String sendDate) { this.sendDate = sendDate; }
+
+    public String getSendTimestamp() { return sendTimestamp; }
+
+    public void setSendTimestamp(String sendTimestamp) { this.sendTimestamp = sendTimestamp; }
+
+    public String getPhone() { return phone; }
+
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public Integer getCost() { return cost; }
+
+    public void setCost(Integer cost) { this.cost = cost; }
+
+    public Integer getSenderId() { return senderId; }
+
+    public void setSenderId(Integer senderId) { this.senderId = senderId; }
+
+    public String getStatusName() { return statusName; }
+
+    public void setStatusName(String statusName) { this.statusName = statusName; }
+
+    public String getMessage() { return message; }
+
+    public void setMessage(String message) { this.message = message; }
+
+    public String getComment() { return comment; }
+
+    public void setComment(String comment) { this.comment = comment; }
+
+    public Integer getType() { return type; }
+
+    public void setType(Integer type) { this.type = type; }
+
+    public Integer getSmsCount() { return smsCount; }
+
+    public void setSmsCount(Integer smsCount) { this.smsCount = smsCount; }
+
     @Override
     public String toString() {
         return "Status{" +
@@ -55,12 +111,12 @@ public class Status {
                 ", sendTimestamp='" + sendTimestamp + '\'' +
                 ", phone='" + phone + '\'' +
                 ", cost=" + cost +
-                ", senderID=" + senderID +
+                ", senderId=" + senderId +
                 ", statusName='" + statusName + '\'' +
                 ", message='" + message + '\'' +
                 ", comment='" + comment + '\'' +
                 ", type=" + type +
-                ", sms_count=" + sms_count +
+                ", smsCount=" + smsCount +
                 '}';
     }
 }
