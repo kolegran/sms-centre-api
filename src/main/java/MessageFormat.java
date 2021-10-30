71200afde3a4dc4df9c250f2d19eff1cf491a638
Pawawudaf <oleksandr.kukotin@gmail.com>
2021-10-31 00:31:27 +0300

    Added Java doc to MessageFormat enum

--- src/main/java/MessageFormat.java
+++ src/main/java/MessageFormat.java
@@ -1,26 +1,120 @@
+/**
+ * Enumerarion used with SMSCServirce class for choice
+ * format of message that will be sent to recipient
+ */
 public enum MessageFormat {
 
+    /**
+     * The argument in HTTP request to API which means choice SMS messaging format
+     */
     SMS("sms=1"),
+
+    /**
+     * The argument in HTTP request to API which means sending a message via Viber
+     */
     VIBER("viber=1"),
+
+    /**
+     * The default value of "flash" flag which means sending simple message
+     */
     FLASH_ORDINARY("flash=0"),
+
+    /**
+     * The argument in HTTP request to API which means sending
+     * a flash message that appears on the recipient's phone screen
+     */
     FLASH("flash=1"),
+
+    /**
+     * The default value of "push" flag which means sending simple message
+     */
     PUSH_ORDINARY("push=0"),
+
+    /**
+     * The argument in HTTP request to API which means sending a push-notification
+     */
     WAP_PUSH("push=1"),
+
+    /**
+     * The default value of "hlr" flag which means sending simple message
+     */
     HLR_ORDINARY("hlr=0"),
+
+    /**
+     * The argument in HTTP request to API which means sending a HLR request
+     */
     HLR("hlr=1"),
+
+    /**
+     * The default value of "bin" flag which means sending simple message
+     */
     BIN_ORDINARY("bin=0"),
+
+    /**
+     * The argument in HTTP request to API which means sending a binary message
+     */
     BIN("bin=1"),
+
+    /**
+     * The argument in HTTP request to API which means sending a binary message in hex format
+     */
     BIN_HEX("bin=2"),
+
+    /**
+     * The default value of "ping" flag which means sending simple message
+     */
     PING_ORDINARY("ping=0"),
+
+    /**
+     * The argument in HTTP request to API which means sending
+     * a ping-SMS that does not appear on the recipient's phone
+     */
     PING_SMS("ping=1"),
+
+    /**
+     * The default value of "mms" flag which means sending simple message
+     */
     MMS_ORDINARY("mms=0"),
+
+    /**
+     * The argument in HTTP request to API which means sending a message with attachment
+     */
     MMS("mms=1"),
+
+    /**
+     * The default value of "mail" flag which means sending simple message
+     */
     E_MAIL_ORDINARY("mail=0"),
+
+    /**
+     * The argument in HTTP request to API which means sending an email message
+     */
     E_MAIL("mail=1"),
+
+    /**
+     * The default value of "call" flag which means sending simple message
+     */
     CALL_ORDINARY("call=0"),
+
+    /**
+     * The argument in HTTP request to API which means sending
+     * a voice-generated message or audio file
+     */
     CALL("call=1"),
+
+    /**
+     * The default value of "soc" flag which means sending simple message
+     */
     SOC_ORDINARY("soc=0"),
+
+    /**
+     * The argument in HTTP request to API which means sending a social network message
+     */
     SOC("soc=1"),
+
+    /**
+     * The value that uses in HTTP request to server for use messages without formatting
+     */
     WITHOUT_FORMAT("")
     ;
 
